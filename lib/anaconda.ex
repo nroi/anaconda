ec7310f7da1c254db780ed71892ecd7b388daa8f
Fabian Muscariello <nroi@mailbox.org>
2017-08-28 00:16:59 +0200

    Introduce function random_string

--- lib/anaconda.ex
+++ lib/anaconda.ex
@@ -1,18 +1,28 @@
 defmodule Anaconda do
-  @moduledoc """
-  Documentation for Anaconda.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Anaconda.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def chars() do
+    first_range = case {'a', 'z'} do
+      {[start], [endd]} -> start..endd
+    end
+    second_range = case {'A', 'Z'} do
+      {[start], [endd]} -> start..endd
+    end
+    third_range = case {'0', '9'} do
+      {[start], [endd]} -> start..endd
+    end
+    chars = Enum.to_list(first_range) ++ Enum.to_list(second_range) ++ Enum.to_list(third_range)
+    List.to_tuple(chars)
   end
+
+  def random_string(len \\ 5) do
+    random_string("", len, chars())
+  end
+
+  def random_string(s, len, _chars) when byte_size(s) >= len, do: s
+  def random_string(s, len, chars) when byte_size(s) < len do
+    idx = :rand.uniform(tuple_size(chars))
+    new_string = s <> to_string([elem(chars, idx - 1)])
+    random_string(new_string, len, chars)
+  end
+
 end
--- lib/anaconda/router.ex
+++ lib/anaconda/router.ex
@@ -1,17 +1,20 @@
 defmodule Anaconda.Router do
   use Plug.Router
+  require Logger
 
   plug :match
   plug :dispatch
 
   get "/" do
+    hostname = :proplists.get_value("shorten-url", conn.req_headers)
+    Logger.debug "Shorten URL: #{inspect hostname}"
     conn
     |> put_resp_content_type("text/plain")
     |> send_resp(200, "Hello, world!")
   end
 
   match _ do
-    send_resp(conn, 404, "oops")
+    send_resp(conn, 404, "Invalid URL")
   end
 end
 
