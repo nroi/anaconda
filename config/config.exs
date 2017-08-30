2d5517e394f6f03255dedae87726cb518481403b
Fabian Muscariello <nroi@mailbox.org>
2017-08-30 09:58:35 +0200

    Store URLs in $XDG_HOME_CACHE or $HOME/.cache

--- config/config.exs
+++ config/config.exs
@@ -1,34 +1,11 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
-
-# You can configure your application as:
-#
 config :anaconda,
-# url_prefix: "https://clkc.cc"
-  url_prefix: "http://localhost",
-  length: 5, # the length of the trailing random string attached to the created URL
-  port: 4072
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:anaconda, :key)
-#
-# You can also configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
+  # the prefix to use when sending the shortened URL in the response body
+  url_prefix: "http://alarm.local:4072",
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+  # the length of the trailing random string attached to the url_prefix
+  length: 5,
+
+  # which port to  listen on
+  port: 4072
--- lib/anaconda/application.ex
+++ lib/anaconda/application.ex
@@ -5,7 +5,15 @@ defmodule Anaconda.Application do
 
   use Application
 
+  def create_cache_dir() do
+    user_cache_dir = System.get_env("XDG_CACHE_HOME") || Path.join(System.user_home(), ".cache")
+    anaconda_cache_dir = Path.join(user_cache_dir, "anaconda")
+    File.mkdir_p!(anaconda_cache_dir)
+    anaconda_cache_dir
+  end
+
   def start(_type, _args) do
+    cache_dir = create_cache_dir()
     # List all child processes to be supervised
     port = Application.fetch_env!(:anaconda, :port)
     children = [
@@ -14,7 +22,7 @@ defmodule Anaconda.Application do
       Plug.Adapters.Cowboy.child_spec(:http, Anaconda.Router, [], [port: port])
     ]
 
-    {:ok, _name} = :dets.open_file(:urls, file: '/tmp/urls')
+    {:ok, _name} = :dets.open_file(:urls, file: to_charlist(Path.join(cache_dir, "urls.dat")))
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
